d28c84c1665f52fc622d9b5a0f36364c0a6d45b4
uzair5520 <uzair2692009@gmail.com>
2026-07-11 22:04:00 +0500

    Delete api directory

--- api/boot.ts
+++
@@ -1,8 +0,0 @@
-import { serve } from "@hono/node-server";
-import app from "./app";
-
-const port = parseInt(process.env.PORT || "4000");
-
-serve({ fetch: app.fetch, port }, () => {
-  console.log(`Chronos API running on http://localhost:${port}/`);
-});
